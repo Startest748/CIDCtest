81b529b64381f74325ef18edc942793ebae9a111
Student.AWS <estardawa@gmail.com>
2025-11-20 21:43:38 -0600

    fixupdate

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-   bucket = "classtest-s3f-backend"
-    key    = "tfstate"
+    bucket = "classtest-s3f-backend"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
